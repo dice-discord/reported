ac3f43381cc3504350b134ce167837dd389d9fd4
Jonah Snider <me@jonahsnider.ninja>
2018-12-18 17:03:32 -0800

    docs: Remove examples

--- examples/batch.js
+++
@@ -1,8 +0,0 @@
-const { Batch } = require('reported');
-
-const batchReport = new Batch({ botlistSpace: 'abcdef123456' }, '123456789');
-
-batchReport.submit({
-  serverCount: 100,
-  shards: [50, 50]
-});
